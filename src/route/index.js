71198c2c7eed9bd4a01088e296ca89c7487e1023
bakulina <bakulinaelena800@gmail.com>
2024-05-02 12:36:05 +0200

    home

--- src/route/index.js
+++ src/route/index.js
@@ -5,84 +5,123 @@ const router = express.Router()
 
 // ================================================================
 
-class User {
-  static #userList = []
-  constructor(login, email, password) {
-    this.login = login
-    this.email = email
-    this.password = password
-    this.id = new Date().getTime()
+class Product {
+  static #list = []
+  static #count = 0
+  constructor(img, title, description, price, category) {
+    this.id = ++Product.#count
+    this.img = img
+    this.title = title
+    this.description = description
+    this.price = price
+    this.category = category
   }
-  static addUser = (user) => {
-    this.#userList.push(user)
-  }
-  static getUserList = () => this.#userList
-
-  static getUserById = (id) =>
-    this.#userList.find((element) => element.id === id)
-
-  static deleteUser = (id) => {
-    const index = this.#userList.findIndex(
-      (value) => value.id === id,
+  static add = (
+    img,
+    title,
+    description,
+    price,
+    category,
+  ) => {
+    const newProduct = new Product(
+      img,
+      title,
+      description,
+      price,
+      category,
     )
-    this.#userList.splice(index, 1)
+    this.#list.push(newProduct)
   }
-  static updateUser = (id, data) => {
-    const user = this.getUserById(id)
-    const { email } = data
-
-    if (user) {
-      if (email) {
-        user.email = email
-      }
-
-      return true
-    } else {
-      return false
-    }
+  static getList = () => {
+    return this.#list
+  }
+  static getById = (id) => {
+    return this.#list.find((product) => product.id === id)
+  }
+  static getRandomList = (id) => {
+    const filteredList = this.#list.filter(
+      (product) => product.id !== id,
+    )
+    const shuffLedlist = filteredList.sort(
+      () => Math.random() - 0.5,
+    )
+    return shuffLedlist.slice(0, 3)
   }
 }
 // ================================================================
 
-router.get('/', function (req, res) {
-  const list = User.getUserList()
-  res.render('index', {
-    style: 'index',
+router.get('/purchase-index', function (req, res) {
+  res.render('purchase-index', {
+    style: 'purchase-index',
     data: {
-      users: {
-        list,
-        isEmpty: list.length === 0,
-      },
+      img: 'https://picsum.photos/200/300',
+      title:
+        "Комп'ютер Artline Gaming(X43v31) AMD Ryzen 5 3600",
+      description:
+        'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+      category: [
+        { id: 1, text: 'готовий до відправки' },
+        { id: 2, text: 'топ продажів' },
+      ],
+      price: 27000,
     },
   })
 })
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер Artline Gaming(X43v31) AMD Ryzen 5 3600",
+  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+  [{ id: 2, text: 'топ продажів' }],
+  20000,
+)
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер Artline Gaming(X43v31) AMD Ryzen 5 3600",
+  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+  [{ id: 1, text: 'готовий до відправки' }],
+  21000,
+)
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер Artline Gaming(X43v31) AMD Ryzen 5 3600",
+  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+  [{ id: 2, text: 'топ продажів' }],
+  20000,
+)
 // ================================================================
-router.post('/user-create', function (req, res) {
-  const { login, email, password } = req.body
-  const user = new User(login, email, password)
-  User.addUser(user)
-  res.render('user-create', {
-    style: 'user-create',
+router.get('/purchase-index', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('purchase-index', {
+    style: 'purchase-index',
+    data: {
+      list: Product.getList(),
+    },
   })
 })
-// ================================================================
-router.get('/user-delete', function (req, res) {
-  const { id } = req.query
-  User.deleteUser(id)
-  res.render('user-delete', {
-    style: 'user-delete',
+
+router.get('/purchase-product', function (req, res) {
+  const id = Number(req.query.id)
+
+  res.render('purchase-product', {
+    style: 'purchase-product',
+    data: {
+      list: Product.getRandomList(id),
+      product: Product.getById(id),
+    },
   })
 })
-// ================================================================
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body
-
-  User.updateUser(Number(id), { email })
-
-  res.render('user-update', {
-    style: 'user-update',
+//==========================
+router.post('/purchase-create', function (req, res) {
+  const id = Number(req.query.id)
+  const amout = Number(req.body.amout)
+  console.log(id, amout)
+  res.render('purchase-product', {
+    style: 'purchase-product',
+    data: {
+      list: Product.getRandomList(id),
+      product: Product.getById(id),
+    },
   })
 })
-
 // Підключаємо роутер до бек-енду
 module.exports = router
