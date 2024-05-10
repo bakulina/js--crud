3c5d96f1ec3f5c29cf8bbe02b2d8433bbd07440f
bakulina <bakulinaelena800@gmail.com>
2024-05-10 20:57:12 +0200

    home bag

--- src/route/index.js
+++ src/route/index.js
@@ -71,8 +71,30 @@ Product.add(
 )
 class Purchase {
   static DELIVERY_PRICE = 150
+  static #BONUS_FACTOR = 0.1
   static #count = 0
   static #list = []
+  static #bonusAccount = new Map()
+  static getBonusBalance = (email) => {
+    return Purchase.#bonusAccount.get(email) || 0
+  }
+  static calcBonusAmount = (value) => {
+    return value * Purchase.#BONUS_FACTOR
+  }
+  static updateBonusBalance = (
+    email,
+    price,
+    bonusUse = 0,
+  ) => {
+    const amount = this.calcBonusAmount(price)
+    const currentBalance = Purchase.getBonusBalance(email)
+    const updateBalance = currentBalance + amount - bonusUse
+
+    Purchase.#bonusAccount.set(email, updateBalance)
+    console.log(email, updateBalance)
+    return amount
+  }
+
   constructor(data, product) {
     this.id = ++Purchase.#count
     this.firstname = data.firstname
@@ -88,6 +110,7 @@ class Purchase {
     this.amount = data.amount
     this.product = product
   }
+
   static add = (...arg) => {
     const newPurchase = new Purchase(...arg)
     this.#list.push(newPurchase)
@@ -137,6 +160,7 @@ Promocode.add('DISCOUNT60', 0.5)
 Promocode.add('SALE', 0.75)
 
 router.get('/', function (req, res) {
+  const id = Number(req.query.id)
   res.render('purchase-index', {
     style: 'purchase-index',
     data: {
@@ -170,7 +194,7 @@ router.post('/purchase-create', function (req, res) {
       data: {
         message: 'Помилка',
         info: 'Некоректна кількість товару',
-        link: '/purchase-product?id=${id}',
+        link: `/purchase-product?id=${id}`,
       },
     })
   }
@@ -182,7 +206,7 @@ router.post('/purchase-create', function (req, res) {
       data: {
         message: 'Помилка',
         info: 'Такого кількості товару нема в налічіі',
-        link: '/purchase-product?=${id}',
+        link: `/purchase-product?=${id}`,
       },
     })
   }
@@ -190,6 +214,7 @@ router.post('/purchase-create', function (req, res) {
 
   const productPrice = product.price * amount
   const totalPrice = productPrice + Purchase.DELIVERY_PRICE
+  const bonus = Purchase.calcBonusAmount(totalPrice)
 
   res.render('purchase-create', {
     style: 'purchase-create',
@@ -197,11 +222,11 @@ router.post('/purchase-create', function (req, res) {
       id: product.id,
       cart: [
         {
-          text: `${product.title}(${amount}шт)`,
-          price: productPrice,
+          text: `${product.title} (${amount} шт)`,
+          price: product.price,
         },
         {
-          text: `Доставка`,
+          text: 'Вартість доставки',
           price: Purchase.DELIVERY_PRICE,
         },
       ],
@@ -209,6 +234,46 @@ router.post('/purchase-create', function (req, res) {
       productPrice,
       deliveryPrice: Purchase.DELIVERY_PRICE,
       amount,
+      bonus,
+    },
+  })
+})
+//====================================================
+
+router.get('/purchase-list', function (req, res) {
+  const id = Number(req.query.id)
+  const list = Purchase.getList()
+
+  res.render('purchase-list', {
+    style: 'purchase-list',
+    title: 'Моі замовлення',
+    data: {
+      purchases: {
+        list,
+      },
+    },
+  })
+})
+router.get('/purchase-info', function (req, res) {
+  const id = Number(req.query.res)
+  const purchase = Purchase.getById(id)
+  const bonus = Purchase.calcBonusAmount(
+    Purchase.TotalPrice,
+  )
+  res.render('purchase-info', {
+    style: 'purchase-info',
+    data: {
+      id: purchase.id,
+      firstname: purchase.firstname,
+      lastname: purchase.lastname,
+      phone: purchase.phone,
+      email: purchase.email,
+      delivery: purchase.delivery,
+      product: purchase.product.title,
+      productPrice: purchase.productPrice,
+      deliveryPrice: purchase.deliveryPrice,
+      totalPrice: purchase.totalPrice,
+      bonus: bonus,
     },
   })
 })
@@ -228,11 +293,13 @@ router.post('/purchase-submit', function (req, res) {
     email,
     phone,
     promocode,
+    bonus,
   } = req.body
 
   const product = Product.getById(id)
   if (!product) {
     return res.render('allert', {
+      style: 'allert',
       data: {
         message: 'Помилка',
         info: 'Товар не знайдено',
@@ -255,11 +322,13 @@ router.post('/purchase-submit', function (req, res) {
   productPrice = Number(productPrice)
   deliveryPrice = Number(deliveryPrice)
   amount = Number(amount)
+  bonus = Number(bonus)
   if (
     isNaN(totalPrice) ||
     isNaN(productPrice) ||
     isNaN(deliveryPrice) ||
-    isNaN(amount)
+    isNaN(amount) ||
+    isNaN(bonus)
   ) {
     return res.render('allert', {
       style: 'allert',
@@ -270,7 +339,7 @@ router.post('/purchase-submit', function (req, res) {
       },
     })
   }
-  if (!firstname || !lastname || !email || !phone) {
+  if ((!firstname, !lastname, !email, !phone)) {
     return res.render('allert', {
       style: 'allert',
       data: {
@@ -280,6 +349,17 @@ router.post('/purchase-submit', function (req, res) {
       },
     })
   }
+  if (bonus || bonus > 0) {
+    const bonusAmount = Purchase.getBonusBalance(email)
+    console.log(bonusAmount)
+    if (bonus > bonusAmount) {
+      bonus = bonusAmount
+    }
+    Purchase.updateBonusBalance(email, totalPrice, bonus)
+    totalPrice -= bonus
+  } else {
+    Purchase.updateBonusBalance(email, totalPrice, 0)
+  }
 
   if (promocode) {
     promocode = Promocode.getByName(promocode)
@@ -287,12 +367,16 @@ router.post('/purchase-submit', function (req, res) {
       totalPrice = Promocode.calc(promocode, totalPrice)
     }
   }
+  if (totalPrice < 0) {
+    totalPrice = 0
+  }
   const purchase = Purchase.add(
     {
       totalPrice,
       productPrice,
       deliveryPrice,
       amount,
+      bonus,
       firstname,
       lastname,
       email,
@@ -303,7 +387,7 @@ router.post('/purchase-submit', function (req, res) {
   )
   console.log(purchase)
 
-  res.render('allerrt', {
+  res.render('allert', {
     style: 'allert',
     data: {
       message: 'успішно',
